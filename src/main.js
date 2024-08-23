335edb162a868ab2409d67b476822c669cdd5c04
unify-z <3336016933@qq.com>
2024-08-23 20:28:31 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// main.js
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '..homepage.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +11,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/rank',
-      name: 'rank',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('./rank.vue')
+      path: '/:pathMatch(.*)*', 
+      name: 'not-found',
+      component: () => import('../homapage.vue') 
     }
   ]
 })
