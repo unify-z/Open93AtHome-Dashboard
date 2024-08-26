dffca3db734b1ad73e873bfafadb2b857af896d9
unify-z <3336016933@qq.com>
2024-08-26 10:02:52 +0800

    删除多余引入

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-import VueWechatTitle from 'vue-wechat-title'
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
  
@@ -11,14 +10,7 @@ const router = createRouter({
   routes
 })
  
-
-
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
-app.use(VueWechatTitle);
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-  }
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
